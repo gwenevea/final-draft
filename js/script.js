e33bde8ebe7eb02c92295c7e86abb4fa98b664d3
Gwenevere McKee <gwenevea@umich.edu>
2022-04-16 13:23:18 -0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,9 +1,32 @@
 //document.querySelector("#darkmode").addEventListener("click", function () {
 	//console.log("Going dark mode");
-	//video.classList.add('dark');
+	//document.body.classList.add('dark-mode');
 //});
 
-function darkMode() {
-    var element = document.body;
-    element.classList.toggle("dark");
-  };
+
+//document.querySelector("#darkmode").addEventListener("click", function () {
+	//console.log("Going dark mode");
+	//document.body.classList.add('dark-mode');
+    //document.querySelector('#darkmode').innerHTML = "Light Mode";
+//});
+
+document.querySelector("#darkmode").addEventListener("click", function () {
+    if ((document.querySelector('#darkmode').innerHTML) == "Dark Mode") {
+        console.log("Going dark mode");
+        document.body.classList.add('dark-mode');
+        document.querySelector('#darkmode').innerHTML = "Light Mode";
+    }
+
+    else {
+        console.log("Going light mode");
+        document.body.classList.remove('dark-mode');
+        document.querySelector('#darkmode').innerHTML = "Dark Mode";
+    }
+
+});
+
+
+//function darkMode() {
+    //var element = document.body;
+    //element.classList.toggle("dark-mode");
+  //};
