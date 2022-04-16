cbba2fc3b30519c63054f621661fc4309cbeb272
Gwenevere McKee <gwenevea@umich.edu>
2022-04-16 13:13:35 -0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+//document.querySelector("#darkmode").addEventListener("click", function () {
+	//console.log("Going dark mode");
+	//video.classList.add('dark');
+//});
+
+function darkMode() {
+    var element = document.body;
+    element.classList.toggle("dark");
+  };
