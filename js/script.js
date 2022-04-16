dbd1fc235e5779b61fbeb329cf26737cda3063c0
Gwenevere McKee <gwenevea@umich.edu>
2022-04-16 13:37:55 -0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,32 +1,31 @@
-//document.querySelector("#darkmode").addEventListener("click", function () {
-	//console.log("Going dark mode");
-	//document.body.classList.add('dark-mode');
-//});
-
-
-//document.querySelector("#darkmode").addEventListener("click", function () {
-	//console.log("Going dark mode");
-	//document.body.classList.add('dark-mode');
-    //document.querySelector('#darkmode').innerHTML = "Light Mode";
-//});
-
 document.querySelector("#darkmode").addEventListener("click", function () {
     if ((document.querySelector('#darkmode').innerHTML) == "Dark Mode") {
         console.log("Going dark mode");
-        document.body.classList.add('dark-mode');
+        const form = document.querySelector("form");
+        form.classList.add('dark-mode');
         document.querySelector('#darkmode').innerHTML = "Light Mode";
     }
 
     else {
         console.log("Going light mode");
-        document.body.classList.remove('dark-mode');
+        const form = document.querySelector("form");
+        form.classList.remove('dark-mode');
         document.querySelector('#darkmode').innerHTML = "Dark Mode";
     }
 
 });
 
+//document.querySelector("#darkmode").addEventListener("click", function () {
+    //if ((document.querySelector('#darkmode').innerHTML) == "Dark Mode") {
+        //console.log("Going dark mode");
+        //document.body.classList.add('dark-mode');
+        //document.querySelector('#darkmode').innerHTML = "Light Mode";
+    //}
 
-//function darkMode() {
-    //var element = document.body;
-    //element.classList.toggle("dark-mode");
-  //};
+    //else {
+        //console.log("Going light mode");
+        //document.body.classList.remove('dark-mode');
+        //document.querySelector('#darkmode').innerHTML = "Dark Mode";
+    //}
+
+//});
